0d5b9345f1c4e11e68e33327d3e7eb27ec548a60
oscarfernandoixcot <oscarfernando.ixcot@gmail.com>
2024-11-01 18:39:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-jenkinsbucket" {
   name                     = "demo-jenkinsbucket-001"
-  project                  = "tt-dev-001"
+  project                  = "radiant-land-440421-h0"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
