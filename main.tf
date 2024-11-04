5407bfd66baff41d7f0404ecc5006a8b1708b108
oscarfernandoixcot <oscarfernando.ixcot@gmail.com>
2024-11-03 18:37:39 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ resource "google_storage_bucket" "my-jenkinsbucket" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket_object" "index" {
+  name   = "index.html"
+  bucket = google_storage_bucket.jenkins_page.name
+  source = "index.html"
+  content_type = "text/html"
+}
