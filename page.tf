ae4a39e6674338f8a2310cd3cda5c683a91b6e5e
oscarfernandoixcot <oscarfernando.ixcot@gmail.com>
2024-11-03 18:47:02 -0600

    Delete page.tf

--- page.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket_object" "index" {
-  name   = "index.html"
-  bucket = google_storage_bucket.jenkins_page.name
-  source = "index.html"
-  content_type = "text/html"
-}
